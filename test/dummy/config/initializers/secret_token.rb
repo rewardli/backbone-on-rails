e5342c7e926bed0312d1820b2c150918224a162a
meleyal <william.meleyal@gmail.com>
2012-02-22 00:13:13 +0100

    add dummy app from plugin generator

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '6d4af1ea5489917af9ad4d22fc8a515534236e20b281bd2e6e426b4f24ed2737d10fcb430825c46312e28ffd28bacf9e74a21db480c5e0dbfae90a38153d8ade'
